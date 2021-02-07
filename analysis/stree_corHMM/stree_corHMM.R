bbbafac7aa5d04cb2bb0e42a26c9b934791d63f3
jordan <jordan.gault@uni-oldenburg.de>
2021-02-07 15:12:52 +0100

    Update filepaths.

--- analysis/stree_corHMM/stree_corHMM.R
+++ analysis/stree_corHMM/stree_corHMM.R
@@ -1,25 +1,22 @@
-#Clear workspace
-rm(list = ls())
-
-# Set working directory
-setwd("~/zoox/results/2019-02-04/")
-
 # Load packages
 library(phytools)
 library(data.table)
 library(phangorn)
 library(corHMM)
-library(parallel)
 
-# Read in molecular tree and traits--------------------------------------------
-stree <- read.nexus("~/zoox/data/2017-12-29/stree_traits/stree.trees")
+# Set working directory
+setwd(here("analysis/stree_corHMM"))
 
-traits <- fread("~/zoox/data/2017-12-29/stree_traits/stree_traits_B_as_Z.csv",
+# Read in tree and traits------------------------------------------------------
+stree <- mtree <- read.nexus(here("data/updated_trees_traits/stree_traits", 
+                                  "stree.trees"))
+
+traits <- fread(here("data/updated_trees_traits/stree_traits",
+                     "stree_traits_B_as_Z.csv"),
                 header = FALSE, col.names = c("taxa", "state"))
 
-
 # Format traits and tip labels-------------------------------------------------
-# Make vector of traits for make.simmap
+# Make vector of traits
 tvec <- traits[, state]
 names(tvec) <- traits[, taxa]
 
@@ -40,7 +37,10 @@ set.seed(1)
 
 stree <- sample(stree, 100, replace = FALSE)
 
-# Run two rate regime across full posterior------------------------------------
+# Run corHMM-------------------------------------------------------------------
+# corHMM has built-in capability for running analyses in parallel. Adjust the 
+# n.cores argument to specify the number of cores to use. 
+
 stree_1rate <- lapply(stree, corHMM, data = tframe[, .(rn, Z)], rate.cat = 1,
                       node.states = "none", nstarts = 120,
                       n.cores = 40)
@@ -67,10 +67,3 @@ stree_4rate <- lapply(stree, corHMM, data = tframe[, .(rn, Z)], rate.cat = 4,
                       n.cores = 40)
 
 saveRDS(stree_4rate, "stree_4rate.rds")
-
-
-stree_5rate <- lapply(stree, corHMM, data = tframe[, .(rn, Z)], rate.cat = 5,
-                      node.states = "none", nstarts = 120,
-                      n.cores = 40)
-
-saveRDS(stree_5rate, "stree_5rate.rds")
