ff5fff9ad16f6e5c3a6bf756df3fd8fa2c9f4b79
jordan <jordan.gault@uni-oldenburg.de>
2021-02-12 10:25:39 +0100

    Remove unneeded plots. Clean up.

--- figures/mtree/mtree_asr_figs.R
+++ figures/mtree/mtree_asr_figs.R
@@ -55,7 +55,7 @@ sctree <- consensus(stree, p = 0.95)
 tips <- sctree$edge[which(sctree$edge[,2] <= length(sctree$tip.label)),2]
 # Get tip labels using index of tips from edge matrix
 tip.index <- sctree$tip.label[tips]
-# Select only tips that are in the molecular tere
+# Select only tips that are in the molecular tree
 tip.index <- tip.index[which(tip.index %in% mctree$tip.label)]
 
 # Make named vector of tips in desired order
@@ -78,7 +78,6 @@ ctree <- rmctree
 ctree <- compute.brlen(ctree)
 
 
-
 # Drop selected tips from tree-------------------------------------------------
 
 # # Plot tree to get nodes that need to be dropped from data
@@ -93,9 +92,9 @@ ctree <- drop.tip(ctree, c("Discosoma", "Rhodactis", "Ricordea_florida"))
 ctree <- drop.tip(ctree, "Anthemiphyllia_patera")
 
 
-
 # Lengthen branches for presentation-------------------------------------------
 
+# Plot with edge labels to see which to change
 # plotTree(ctree, ftype = "off", type = "fan")
 # edgelabels(frame = "n")
 
@@ -141,6 +140,7 @@ ctree$edge.length[336] <- ctree$edge.length[336] + 0.01
 ctree$edge.length[335] <- ctree$edge.length[335] - 0.01
 ctree$edge.length[361] <- ctree$edge.length[361] + 0.01
 
+
 # Format asr for plotting------------------------------------------------------
 
 ### Replace ctree.nodes in f.anc[[2]] with updated nodes and drop data
@@ -159,101 +159,42 @@ setkey(fanc2, ctree.nodes)
 # Remove nodes that were dropped from tree
 fanc2 <- fanc2[-which(ctree.nodes %in% c(579, 858, 859)),]
 
-### Format f.anc to look like asrs generated from ancRECON so I can use the 
-### treePaint function I wrote
-# 2 rate
+### Format f.anc to look like asr's generated from ancRECON so I can use the 
+### function I wrote
 fanc2[, ctree.nodes := NULL]
 fanc2 <- list(fanc2)
 names(fanc2) <- "lik.anc.states"
 
 
-
 # Paint branches---------------------------------------------------------------
 
-# Make color vectors for plotting combined rate cats
-# 1 rate
-r1.cols <- c("grey", "#084594", "#E6AB02")
-names(r1.cols) <- c(1, "A", "Z")
+### Paint tree with rate cats based on max p at each node
+m2.asr <- maxPaint(tree = ctree, anc = fanc2, rate.cat = 2)
 
-# f2.cols <- c("grey", "grey", "grey", "red", "red")
-# names(f2.cols) <- c(1, "AS", "ZS", "AF", "ZF")
 
-# Fast categories
-# rc.cols <- c("grey", "red")
-# names(rc.cols) <- c(1, "F")
-fast.cols <- c("#bdbdbd", "#fc9272")
-names(fast.cols) <- c("Stable", "Labile")
+### Plot-----------------------------------------------------------------------
 
-# Regular colors for plotting all rate cats
-# 2 rate
+# Define branch colors for plotting
 r2.cols <- c("grey", "#084594", "#E6AB02", "#1B9E77", "#F781BF")
 names(r2.cols) <- c(1, "AS", "ZS", "AF", "ZF")
 
-# Make vector of trait colors
+# Make vector of trait colors for tips
 trait.cols <- traits[, state]
 names(trait.cols) <- traits[, taxa]
 trait.cols[trait.cols == "A"] <- "#084594"
 trait.cols[trait.cols == "Z"] <- "#E6AB02"
 
 
-### Paint trees with azoox/zoox recons
-# 2 rate
-og2.asr <- azPaint(ctree, anc = fanc2$lik.anc.states, rate.cat = 2)
-
-### Paint summed fast categories on trees
-# 2 rate
-rc2.asr <- maxFast(ctree, fanc2$lik.anc.states, rate.cat = 2)
-
-### Paint trees with rate categories
-# 2 rate
-f2.asr <- treePaint(tree = ctree, anc = fanc2, rate.cat = 2)
-
-### Paint tree with rate cats based on max p at each node
-m2.asr <- maxPaint(tree = ctree, anc = fanc2, rate.cat = 2)
-
-### Paint tree with different cutoff values
-f5.asr <- ptreePaint(tree = ctree, anc = fanc2, rate.cat = 2, p = 0.5)
-
-### Plot-----------------------------------------------------------------------
-
-# Plot binary asr
-pdf(file = "binary_asr.pdf", width = 5, height = 5)
-plotSimmap(og2.asr$tree, type = "fan", ftype = "off", r1.cols, lwd = 0.5)
-dev.off()
-
-# Plot binary with names for reference 
-pdf(file = "binary_reference.pdf", width = 60.84, height = 40.63)
-plotSimmap(og2.asr$tree, type = "fan", fsize = 0.3, r1.cols, lwd = 0.5)
-nodelabels(pie = as.matrix(og2.asr$nodeprobs), cex = 0.05,
-           piecol = r1.cols[-1])
-tiplabels(pch = 19, cex = 0.5, col = trait.cols[ctree$tip.label])
-dev.off()
-
-# Plot summed fast cats
-pdf(file = "fast_cats.pdf", width = 5, height = 5)
-plotSimmap(rc2.asr, type = "fan", ftype = "off", fast.cols, lwd = 0.5)
-dev.off()
-
-# Plot all 4 categories
-pdf(file = "all4_asr.pdf", width = 5, height = 5)
-plotSimmap(f2.asr, type = "fan", ftype = "off", r2.cols, lwd = 0.5)
-dev.off()
-
-pdf(file = "all4_maxp_asr.pdf", width = 5, height = 5)
+# Plot based on maximum probability at nodes
+pdf(file = "mtree_consensus_asr.pdf", width = 5, height = 5)
 plotSimmap(m2.asr, type = "fan", ftype = "off", r2.cols, lwd = 0.5)
 dev.off()
 
-pdf(file = "all4_maxp_reference.pdf", width = 60.84, height = 40.63)
+# Plot large figure with tip labels and pie charts at nodes
+pdf(file = "mtree_consensus_asr_tiplabels.pdf", width = 60.84, height = 40.63)
 plotSimmap(m2.asr, type = "fan", fsize = 0.3, r2.cols, lwd = 1)
 nodelabels(pie = as.matrix(fanc2$lik.anc.states), cex = 0.05,
            piecol = r2.cols[-1])
 tiplabels(pch = 19, cex = 0.5, col = trait.cols[ctree$tip.label])
 dev.off()
-
-
-
-# plotSimmap(f5.asr, type = "fan", fsize = 0.3, r2.cols, lwd = 1)
-# nodelabels(pie = as.matrix(fanc2$lik.anc.states), cex = 0.05,
-#            piecol = c("#0F4FA8", "#FF9F00", "#00A480", "red"))
-# tiplabels(pch = 21, bg = trait.cols[ctree$tip.label], cex = 0.5, lwd = 0.00001)
 
