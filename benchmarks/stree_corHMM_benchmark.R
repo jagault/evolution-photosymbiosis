a05e829bf3a66084a7c23f69fa5b38fabb846e40
jordan <jordan.gault@uni-oldenburg.de>
2021-05-05 14:02:48 +0200

    Compare internal restarts with loop alternative.

--- benchmarks/stree_corHMM_benchmark.R
+++ benchmarks/stree_corHMM_benchmark.R
@@ -37,20 +37,55 @@ tframe <- data.table(tframe, keep.rownames = TRUE)
 # corHMM has built-in capability for running analyses in parallel. Adjust the 
 # n.cores argument to specify the number of cores to use. 
 
-b1 <- system.time(
+t1 <- system.time(
   stree_1rate <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 1,
                         node.states = "none", nstarts = 1,
                         n.cores = 1)
 )
 
-b2 <- system.time(
+t2 <- system.time(
   stree_2rate <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 2,
                         node.states = "none", nstarts = 1,
                         n.cores = 1)
 )
 
-b3 <- system.time(
+t3 <- system.time(
   stree_3rate <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 3,
                         node.states = "none", nstarts = 1,
                         n.cores = 1)
+)
+
+# Test different approaches to random restarts---------------------------------
+
+t1r <- system.time(
+  stree_1rate_r <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 1,
+                          node.states = "none", nstarts = 20,
+                          n.cores = 1)
+)
+
+st1 <- list()
+tloop1 <- system.time(
+for (i in 1:19) {
+  st1[[i]] <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 1,
+                     node.states = "none", nstarts = 1,
+                     n.cores = 1)
+}
+)
+
+
+
+
+t2r <- system.time(
+  stree_2rate_r <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 2,
+                          node.states = "none", nstarts = 5,
+                          n.cores = 1)
+)
+
+st2 <- list()
+tloop2 <- system.time(
+  for (i in 1:4) {
+    st2[[i]] <- corHMM(stree[[1]], data = tframe[, .(rn, Z)], rate.cat = 2,
+                       node.states = "none", nstarts = 1,
+                       n.cores = 1)
+  }
 )
