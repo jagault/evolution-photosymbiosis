2b8dede13b8534d7157f3e4c9b24e78be22c735e
jordan <jordan.gault@uni-oldenburg.de>
2021-06-04 15:30:58 +0200

    Remove section assessing monophyly of Leptoseris.

--- leptoseris_analysis/data/mtree_leptoseris/mtree_add_lt.R
+++ leptoseris_analysis/data/mtree_leptoseris/mtree_add_lt.R
@@ -7,6 +7,7 @@ library(here)
 # Set working directory
 setwd(here("leptoseris_analysis/data/mtree_leptoseris"))
 
+
 # Read in tree and traits------------------------------------------------------
 mtree <- read.nexus(here("data/updated_trees_traits/mtree_traits", 
                          "mtree.trees"))
@@ -14,6 +15,7 @@ mtree <- read.nexus(here("data/updated_trees_traits/mtree_traits",
 traits <- fread(here("data/updated_trees_traits/mtree_traits",
                      "mtree_traits_B_as_Z.csv"),
                 header = FALSE, col.names = c("taxa", "state"))
+
 
 # Add leptoseris troglodyta to tree--------------------------------------------
 mtree <- .uncompressTipLabel(mtree)
@@ -24,27 +26,13 @@ for (i in 1:length(mtree)) {
                                      where = "random")
 }
 
+
 # Add Leptoseris troglodyta to traits-------------------------------------------
 lt <- data.table("taxa" = c("Leptoseris_troglodyta"),
                    "state" = c("A"))
   
 traits <- rbind(traits, lt)
 
-# Test monophyly of Leptoseris--------------------------------------------------
-# ll <- copy(traits)
-# ll[, c("genus", "s1", "s2") := tstrsplit(taxa, "_")]
-# 
-# is.monophyletic(mtree[[1]], ll[genus == "Leptoseris", taxa], plot = T)
-# 
-# m <- c()
-# for (i in 1:length(mtree)){
-#   m[i] <- is.monophyletic(mtree[[i]], ll[genus == "Leptoseris", taxa])
-# }
-# m
-# unique(m)
-# 
-# plot(mtree[[1]], cex = 0.2)
-# edgelabels(frame = "n")
 
 # Write to file-----------------------------------------------------------------
 write.nexus(mtree, file = "mtree_lt.trees")
--- leptoseris_analysis/data/stree_leptoseris/stree_add_lt.R
+++ leptoseris_analysis/data/stree_leptoseris/stree_add_lt.R
@@ -7,6 +7,7 @@ library(here)
 # Set working directory
 setwd(here("leptoseris_analysis/data/stree_leptoseris"))
 
+
 # Read in tree and traits------------------------------------------------------
 stree <- read.nexus(here("data/updated_trees_traits/stree_traits", 
                          "stree.trees"))
@@ -14,6 +15,7 @@ stree <- read.nexus(here("data/updated_trees_traits/stree_traits",
 traits <- fread(here("data/updated_trees_traits/stree_traits",
                      "stree_traits_B_as_Z.csv"),
                 header = FALSE, col.names = c("taxa", "state"))
+
 
 # Add leptoseris troglodyta to tree--------------------------------------------
 stree <- .uncompressTipLabel(stree)
@@ -24,24 +26,13 @@ for (i in 1:length(stree)) {
                                      where = "random")
 }
 
+
 # Add Leptoseris troglodyta to traits-------------------------------------------
 lt <- data.table("taxa" = c("Leptoseris_troglodyta"),
                  "state" = c("A"))
 
 traits <- rbind(traits, lt)
 
-# Test monophyly of Leptoseris--------------------------------------------------
-# ll <- copy(traits)
-# ll[, c("genus", "s1", "s2") := tstrsplit(taxa, "_")]
-# 
-# is.monophyletic(stree[[1]], ll[genus == "Leptoseris", taxa], plot = T)
-# 
-# m <- c()
-# for (i in 1:length(stree)){
-#   m[i] <- is.monophyletic(stree[[i]], ll[genus == "Leptoseris", taxa])
-# }
-# m
-# unique(m)
 
 # Write to file-----------------------------------------------------------------
 write.nexus(stree, file = "stree_lt.trees")
