a3df2069ef9398fcc98276ac64fa7f1d70f41817
jordan <jordan.gault@uni-oldenburg.de>
2021-06-20 15:48:46 +0200

    AIC summary for stree.

---
+++ analysis_rerun/stree_corHMM/aic-summary.R
@@ -0,0 +1,38 @@
+# Load packages
+library(phytools)
+library(data.table)
+library(phangorn)
+library(here)
+
+# Set working directory
+setwd(here("analysis_rerun/stree_corHMM/"))
+
+# Read in files
+stree.1rate <- readRDS("stree_1rate/stree-1rate.rds")
+stree.2rate <- readRDS("stree_2rate/stree-2rate.rds")
+stree.3rate <- readRDS("stree_3rate/stree-3rate.rds")
+stree.4rate <- readRDS("stree_4rate/stree-4rate.rds")
+
+# Select AICc scores
+aic.1rate <- unlist(sapply(stree.1rate, "[", "AICc"))
+aic.2rate <- unlist(sapply(stree.2rate, "[", "AICc"))
+aic.3rate <- unlist(sapply(stree.3rate, "[", "AICc"))
+aic.4rate <- unlist(sapply(stree.4rate, "[", "AICc"))
+
+# Summarize
+summary(aic.1rate)
+summary(aic.2rate)
+summary(aic.3rate)
+summary(aic.4rate)
+
+# Calculate standard error
+std <- function(x) sd(x)/sqrt(length(x))
+
+std(aic.1rate)
+std(aic.2rate)
+std(aic.3rate)
+std(aic.4rate)
+
+# Boxplot
+aic <- cbind(aic.1rate, aic.2rate, aic.3rate, aic.4rate)
+boxplot(aic)
