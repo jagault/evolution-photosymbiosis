4bddd67396b4e64f24092ef6d6fa57a391ca919c
jordan <jordan.gault@uni-oldenburg.de>
2021-06-20 14:43:10 +0200

    Script for processing corHMM results for mtree-3rate.

---
+++ analysis_rerun/mtree_corHMM/mtree_3rate/mtree-3rate-process.R
@@ -0,0 +1,37 @@
+# Load packages
+library(phytools)
+library(data.table)
+library(phangorn)
+library(here)
+
+# Set working directory
+setwd(here("analysis_rerun/mtree_corHMM/mtree_3rate/"))
+
+# Define path to results, will be used to read in files in for-loop below
+path <- c("~/Dropbox/osg_results/stree/mtree/mtree_3rate")
+
+# Initialize vector for number of trees
+t <- c(1:3361)
+
+# Create empty vectors and lists to hold results
+files <- c()
+results <- list()
+maxl <- list()
+liks <- c()
+reps <- c()
+
+# Loop through results and save max-likelihood results for each tree
+for (i in 1:length(t)){
+  files <- list.files(path = paste(path, "/3-rate_", i, sep = ""), 
+                      pattern = ".rds", full.names = T)
+  
+  results <- lapply(files, readRDS)
+  
+  liks <- sapply(results, "[", "loglik", simplify = T)
+  
+  maxl[[i]] <- results[[which.max(liks)]]
+  reps[i] <- length(liks)
+}
+
+# Save list of results 
+saveRDS(maxl, "mtree-3rate.rds")
