bf97ff0fe8a3a8d334d385dbeb3fe86b20e14f1d
jordan <jordan.gault@uni-oldenburg.de>
2021-06-20 15:56:18 +0200

    Add AICc weights.

--- leptoseris_analysis/analysis/stree_corHMM/aic-summary.R
+++ leptoseris_analysis/analysis/stree_corHMM/aic-summary.R
@@ -1,7 +1,4 @@
 # Load packages
-library(phytools)
-library(data.table)
-library(phangorn)
 library(here)
 
 # Set working directory
@@ -36,3 +33,25 @@ std(aic.4rate)
 # Boxplot
 aic <- cbind(aic.1rate, aic.2rate, aic.3rate, aic.4rate)
 boxplot(aic)
+
+# AICc weights
+daic1 <- 346.0 - 287.8
+daic2 <- 298.3 - 287.8
+daic3 <- 287.8 - 287.8
+daic4 <- 297.8 - 287.8
+
+rl1 <- exp(-.5*daic1)
+rl2 <- exp(-.5*daic2)
+rl3 <- exp(-.5*daic3)
+rl4 <- exp(-.5*daic4)
+
+aicw1 <- rl1/sum(rl1, rl2, rl3, rl4)
+aicw2 <- rl2/sum(rl1, rl2, rl3, rl4)
+aicw3 <- rl3/sum(rl1, rl2, rl3, rl4)
+aicw4 <- rl4/sum(rl1, rl2, rl3, rl4)
+
+# Summary table
+aicw1
+aicw2
+aicw3
+aicw4
