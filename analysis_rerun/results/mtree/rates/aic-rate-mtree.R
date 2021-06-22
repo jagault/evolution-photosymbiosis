928be86f84045ce919859e2b0cf9f82c9230b338
jordan <jordan.gault@uni-oldenburg.de>
2021-06-22 16:45:27 +0200

    Script for summarizing mtree rate estimates.

---
+++ analysis_rerun/results/mtree/rates/aic-rate-mtree.R
@@ -0,0 +1,151 @@
+# Load packages
+library(data.table)
+library(ggplot2)
+library(here)
+
+# Set working directory
+setwd(here("analysis_rerun/results/mtree/rates/"))
+
+# Suppress scientific notation
+options(scipen = 999)
+
+# Source helper functions
+source(here("R/aic-rate-summary.R"))
+
+# Read in corHMM runs----------------------------------------------------------
+r1 <- readRDS(here("analysis_rerun/mtree_corHMM/mtree_1rate/mtree-1rate.rds"))
+r2 <- readRDS(here("analysis_rerun/mtree_corHMM/mtree_2rate/mtree-2rate.rds"))
+r3 <- readRDS(here("analysis_rerun/mtree_corHMM/mtree_3rate/mtree-3rate.rds"))
+
+
+# Summarize rates and model selection------------------------------------------
+sr1 <- corSumm(r1, rate.cat = 1)
+sr2 <- corSumm(r2, rate.cat = 2)
+sr3 <- corSumm(r3, rate.cat = 3)
+
+### Model selection~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AICc weights
+daic1 <- 335.5603 - 277.5107
+daic2 <- 286.5664 - 277.5107
+daic3 <- 277.5107 - 277.5107
+
+rl1 <- exp(-.5*daic1)
+rl2 <- exp(-.5*daic2)
+rl3 <- exp(-.5*daic3)
+
+aicw1 <- rl1/sum(rl1, rl2, rl3, rl4)
+aicw2 <- rl2/sum(rl1, rl2, rl3, rl4)
+aicw3 <- rl3/sum(rl1, rl2, rl3, rl4)
+
+# Summary table
+aic.table <- data.table(model = c("TH", "HRM+2", "HRM+3"),
+                        num.rate.class = c(1, 2, 3),
+                        num.param = c(2, 8, 14),
+                        mean.AICc = c(round(sr1$aic.summ[, mean], 3),
+                                      round(sr2$aic.summ[, mean], 3),
+                                      round(sr3$aic.summ[, mean], 3)),
+                        AICc.weight.percent = c(round(aicw1*100, 3), 
+                                                round(aicw2*100, 3), 
+                                                round(aicw3*100, 3)))
+aic.table
+fwrite(aic.table, file = "aic_weights.csv")
+
+### Model selection plots-------------------------------------------------------
+aics <- cbind(sr1$aic, sr2$aic, sr3$aic)
+setnames(aics, c("TH", "HRM+2", "HRM+3"))
+aics <- melt(aics, variable = "model", value = "AICc")
+
+
+# pdf(file = "aic_histogram.pdf", width = 6.83, height = 4.31)
+ggplot(aics) + geom_histogram(aes(x = AICc, fill = model), 
+                              alpha = .5, binwidth = 2)
+# dev.off()
+
+
+### Line plot of mean aics
+maic <- data.table(aic = c(sr1$aic.summ[, mean], sr2$aic.summ[, mean], 
+                           sr3$aic.summ[, mean]),
+                   stderr = c(sr1$aic.summ[, std.err], sr2$aic.summ[, std.err],
+                              sr3$aic.summ[, std.err]),
+                   model = c("TH", "HRM+2", "HRM+3"))
+
+
+
+# pdf(file = "aic_curve.pdf", width = 6.83, height = 4.31)
+ggplot(maic, aes(x = model, y = -aic, group = 1)) + geom_point() + geom_path() +
+  scale_x_discrete(limits= c("TH", "HRM+2", "HRM+3")) + 
+  geom_errorbar(aes(ymin = -aic - stderr, ymax = -aic + stderr), width = 0.1) +
+  xlab("Model") + 
+  ylab("-AIC") + 
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# dev.off()
+
+### Line plot for log-likelihoods
+lik1 <- unlist(sapply(r1, "[", "loglik"))
+lik2 <- unlist(sapply(r2, "[", "loglik"))
+lik3 <- unlist(sapply(r3, "[", "loglik"))
+
+loglik <- data.table("TH" = lik1, "HRM+2" = lik2, "HRM+3" = lik3)
+
+loglik <- melt(loglik, value = "loglik", variable.name = "model")
+
+loglik <- loglik[, .(loglik = mean(loglik), std.err = sd(loglik)/.N), 
+                 by = "model"]
+
+# pdf(file = "likelihood_curve.pdf", width = 6.83, height = 4.31)
+ggplot(loglik, aes(x = model, y = loglik, group = 1)) + geom_point() + 
+  geom_path() +
+  scale_x_discrete(limits= c("TH", "HRM+2", "HRM+3")) + 
+  geom_errorbar(aes(ymin = loglik - std.err, ymax = loglik + std.err), 
+                width = 0.1) + 
+  ylab("-LnLik") + 
+  xlab("Model") + 
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# dev.off()
+
+
+### Rates-----------------------------------------------------------------------
+sr1q <- apply(sr1$par[,2:ncol(sr1$par)], 2, bootMed, n = 100000)
+sr1q <- data.table(t(sr1q), keep.rownames = T)
+
+
+sr2q <- apply(sr2$par[,2:ncol(sr2$par)], 2, bootMed, n = 100000)
+sr2q <- data.table(t(sr2q), keep.rownames = T)
+
+
+sr3q <- apply(sr3$par[,2:ncol(sr3$par)], 2, bootMed, n = 100000)
+sr3q <- data.table(t(sr3q), keep.rownames = T)
+
+
+sr3q <- apply(sr3$par[,2:ncol(sr3$par)], 2, bootMed, n = 100000)
+sr3q <- data.table(t(sr3q), keep.rownames = T)
+
+
+setnames(sr1q, c("transition", "lci", "median", "uci"))
+setnames(sr2q, c("transition", "lci", "median", "uci"))
+setnames(sr3q, c("transition", "lci", "median", "uci"))
+
+
+sr1q[, c("lci", "median", "uci") := list(round(lci*1000, 3), 
+                                         round(median*1000, 3), 
+                                         round(uci*1000, 3))]
+
+sr2q[, c("lci", "median", "uci") := list(round(lci*1000, 3), 
+                                         round(median*1000, 3), 
+                                         round(uci*1000, 3))]
+
+sr3q[, c("lci", "median", "uci") := list(round(lci*1000, 3), 
+                                         round(median*1000, 3), 
+                                         round(uci*1000, 3))]
+
+
+sr1q[, c("minus", "plus") := list(round(median-lci, 3), round(uci-median, 3))]
+sr2q[, c("minus", "plus") := list(round(median-lci, 3), round(uci-median, 3))]
+sr3q[, c("minus", "plus") := list(round(median-lci, 3), round(uci-median, 3))]
+
+fwrite(sr1q, file = "1rate_rates.csv")
+fwrite(sr2q, file = "2rate_rates.csv")
+fwrite(sr3q, file = "3rate_rates.csv")
+fwrite(sr4q, file = "4rate_rates.csv")
