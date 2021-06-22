87af51d44a166f225b2fd3d75f0b79106099871e
jordan <jordan.gault@uni-oldenburg.de>
2021-06-22 14:30:40 +0200

    Process stree asr results.

---
+++ analysis_rerun/stree_asr/stree-asr-process.R
@@ -0,0 +1,26 @@
+# Load packages
+library(here)
+
+# Set working directory
+setwd(here("analysis_rerun/stree_asr"))
+
+# Define path to results, will be used to read in files in for-loop below
+path <- c("~/Dropbox/osg_results/ASR/stree_asr/")
+
+# Initialize vector for number of trees
+t <- c(1:1000)
+
+# Create empty vectors and lists to hold results
+files <- c()
+results <- list()
+
+# Loop through asr results and add them to a single list
+for (i in 1:length(t)){
+  files <- list.files(path = paste(path, "stree_asr_", i, sep = ""),
+                      pattern = ".rds", full.names = T)
+  
+  results[[i]] <- readRDS(files)
+}
+
+# Save list of results 
+saveRDS(results, "stree-asr.rds")
